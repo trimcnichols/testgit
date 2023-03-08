7ec7e364e85cec93dc97b4490b660387d048dece
trimcnichols <118218975+trimcnichols@users.noreply.github.com>
2023-03-08 17:51:19 -0500

    Update driver.java

--- src/main/java/com/revature/driver.java
+++ src/main/java/com/revature/driver.java
@@ -2,7 +2,7 @@ package com.revature;
 
 public class driver {
     public static void main(String[] args) {
-        System.out.printf("lolo");
+        System.out.printf("lolo where we can go anywhere");
         System.out.println("hello world from" + args[0]);
     }
 }
