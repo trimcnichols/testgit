b611b7f85e8a8d3ddeaf7b3420b230489322156d
trimcnichols <118218975+trimcnichols@users.noreply.github.com>
2023-03-08 18:53:28 -0500

    Update driver.java

--- src/main/java/com/revature/driver.java
+++ src/main/java/com/revature/driver.java
@@ -2,7 +2,7 @@ package com.revature;
 
 public class driver {
     public static void main(String[] args) {
-        System.out.printf("lolo where we can go anywhere");
+        System.out.printf("I am from testgit file");
         System.out.printf("add from local git");
         System.out.println("hello world from" + args[0]);
     }
