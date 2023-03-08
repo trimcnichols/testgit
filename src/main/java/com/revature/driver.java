0fb6c52ce4fded1e5afa7279734ed733ae62c46a
tetap semangat <trimcnichols@outlook.com>
2023-03-08 17:29:47 -0500

    add scr file for git

---
+++ src/main/java/com/revature/driver.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class driver {
+    public static void main(String[] args) {
+        System.out.printf("lolo");
+        System.out.println("hello world from" + args[0]);
+    }
+}
